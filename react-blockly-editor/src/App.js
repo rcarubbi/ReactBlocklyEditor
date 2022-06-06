907785dd7f01b22260eb53984de5b76315180f51
Raphael Carubbi Neto <rcarubbi@gmail.com>
2022-06-06 16:48:00 +0100

    Load blocksets dynamically

--- react-blockly-editor/src/App.js
+++ react-blockly-editor/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import BlocklyEditor from './Blockly/BlocklyEditor.js';
+import {useState } from 'react';
 
 function App() {
+  const [blockSet, setBlockSet] = useState("JsonBotDefinition");
   return (
-    <div id="app">
-      <BlocklyEditor></BlocklyEditor>
+    <div id="app" onChange={e => setBlockSet(e.target.value)}>
+      <select>
+        <option value="JsonBotDefinition">Json bot definition</option>
+        <option value="RulesEngine">Rules Engine</option>
+      </select>
+      <BlocklyEditor blockSet={blockSet}></BlocklyEditor>
     </div>
   );
 }
