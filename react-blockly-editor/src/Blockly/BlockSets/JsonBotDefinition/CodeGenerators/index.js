f32199dc313a0bfe0159c66d3d8cc541df15063e
Raphael Carubbi Neto <rcarubbi@gmail.com>
2022-06-13 01:30:25 +0100

    removed old files

--- react-blockly-editor/src/Blockly/BlockSets/JsonBotDefinition/CodeGenerators/index.js
+++
@@ -1,5 +0,0 @@
-import JsonBotDefinition from "./JsonBotDefinition.js";
-import FlowChart from "./FlowChart.js";
-
-const codeGenerators = [JsonBotDefinition, FlowChart];
-export default codeGenerators;
--- react-blockly-editor/src/Blockly/BlockSets/RulesEngine/CodeGenerators/index.js
+++
@@ -1,5 +0,0 @@
-import RulesEngine from "./RulesEngine.js";
- 
-
-const codeGenerators = [RulesEngine];
-export default codeGenerators;
